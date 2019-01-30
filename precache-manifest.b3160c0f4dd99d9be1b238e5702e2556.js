5dfbe76658e37842a6d063ef192d670e05a4f2f1
Sravani <sravani@docsapp.in>
2019-01-30 22:04:17 +0530

    Updates

---
+++ precache-manifest.b3160c0f4dd99d9be1b238e5702e2556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "04c370a72044cee8f4c2",
+    "url": "/static/js/main.04c370a7.chunk.js"
+  },
+  {
+    "revision": "fc393ba31d8fd790840d",
+    "url": "/static/js/1.fc393ba3.chunk.js"
+  },
+  {
+    "revision": "04c370a72044cee8f4c2",
+    "url": "/static/css/main.e1bf351c.chunk.css"
+  },
+  {
+    "revision": "fc393ba31d8fd790840d",
+    "url": "/static/css/1.38dbc0d4.chunk.css"
+  },
+  {
+    "revision": "3526af092e48d8210d913e8dd1d7415d",
+    "url": "/index.html"
+  }
+];
